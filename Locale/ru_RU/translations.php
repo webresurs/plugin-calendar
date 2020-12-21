d0a0b7cbd51d3964bca403ef9c824beb8931539f
webresurs <76422777+webresurs@users.noreply.github.com>
2020-12-21 14:05:29 +0500

    Update translations.php
      add
        'Switch to the list view' => 'Перейти к просмотру списка',
        'Show subtasks based on the time tracking' => 'Показывать подзадачи на основе учета времени',
        'Show tasks based on the creation date' => 'Показывать задачи по дате создания',

--- Locale/ru_RU/translations.php
+++ Locale/ru_RU/translations.php
@@ -6,7 +6,9 @@ return array(
     'Project calendar view' => 'Вид календаря проекта',
     'User calendar view' => 'Просмотреть календарь пользователя',
     'My calendar' => 'Мой календарь',
-    'Switch to the list view' => 'Переключиться в режим списка',
+    'Switch to the list view' => 'Перейти к просмотру списка',
+    'Show subtasks based on the time tracking' => 'Показывать подзадачи на основе учета времени',
+    'Show tasks based on the creation date' => 'Показывать задачи по дате создания',
     'Show tasks based on the start date' => 'Показать задачи в зависимости от даты начала',
     'Subtasks time tracking' => 'Отслеживание времени подзадач',
 );
